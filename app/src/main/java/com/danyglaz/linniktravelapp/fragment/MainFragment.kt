e702f82382b8f07d1e05e9293c08014e36c584b5
Данила Глазунов <glazdan17@gmail.com>
2025-02-09 19:55:00 +0300

    git add fragmentSelection

--- app/src/main/java/com/danyglaz/linniktravelapp/fragment/MainFragment.kt
+++ app/src/main/java/com/danyglaz/linniktravelapp/fragment/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.findFragment
 import androidx.navigation.fragment.findNavController
 import com.danyglaz.linniktravelapp.R
@@ -22,7 +23,7 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mainFragment=view.findViewById<Button>(R.id.start)
+        val mainFragment=view.findViewById<TextView>(R.id.start)
         val controller=findNavController()
         mainFragment.setOnClickListener {controller.navigate(R.id.selectionFragment)  }
 
