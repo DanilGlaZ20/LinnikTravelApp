5c997f23fe563011ae2507e9293f9164764c3de9
Данила Глазунов <glazdan17@gmail.com>
2025-02-19 11:02:35 +0300

    git add fragment_second_route.xml

--- app/src/main/java/com/danyglaz/linniktravelapp/fragment/ParkGpsFragment.kt
+++ app/src/main/java/com/danyglaz/linniktravelapp/fragment/ParkGpsFragment.kt
@@ -1,86 +1,30 @@
 package com.danyglaz.linniktravelapp.fragment
 
-import android.content.Context
-import android.icu.util.MeasureUnit.POINT
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.danyglaz.linniktravelapp.R
-import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.RequestPoint
 import com.yandex.mapkit.RequestPointType
 import com.yandex.mapkit.directions.DirectionsFactory
-import com.yandex.mapkit.directions.driving.DrivingOptions
-import com.yandex.mapkit.directions.driving.DrivingRoute
-import com.yandex.mapkit.directions.driving.DrivingRouter
-import com.yandex.mapkit.directions.driving.DrivingRouterType
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
 import com.yandex.mapkit.mapview.MapView
-import com.yandex.mapkit.directions.driving.DrivingSession
-import com.yandex.mapkit.directions.driving.VehicleOptions
-import com.yandex.mapkit.directions.driving.VehicleType
-import com.yandex.mapkit.map.MapObjectCollection
-import com.yandex.mapkit.transport.masstransit.RouteMetadata
+import com.yandex.mapkit.transport.TransportFactory
+import com.yandex.mapkit.transport.masstransit.Route
+import com.yandex.mapkit.transport.masstransit.Session
+import com.yandex.mapkit.transport.masstransit.TimeOptions
 import com.yandex.runtime.Error
 import com.yandex.runtime.network.NetworkError
 import com.yandex.runtime.network.RemoteError
-/*class ParkGpsFragment:Fragment(),DrivingSession.DrivingRouteListener{
-    private val START_LOCATION=Point(55.752329, 37.609381)
-    private val FINISH_LOCATION=Point(55.710449, 37.517074)
-    private val SCREEN_CENTER=Point(
-            (START_LOCATION.latitude+FINISH_LOCATION.latitude)/2,
-            (START_LOCATION.longitude+FINISH_LOCATION.longitude)/2
 
-    )
+class ParkGpsFragment : Fragment(), Session.RouteListener {
     private lateinit var mapView: MapView
-    private var mapObjects:MapObjectCollection?=null
-    private var drivingRouter:DrivingRouter?=null
-    private var drivingSession:DrivingSession?=null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        MapKitFactory.initialize(requireContext())
-        DirectionsFactory.initialize(requireContext())
-        drivingRouter=DirectionsFactory.getInstance().createDrivingRouter.requestRoutes(
-
-        )
-        mapObjects=mapView.map.mapObjects.addCollection()
-        sumbitRequest
-
-    }
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_park_gps, container, false)
-        mapView = rootView.findViewById(R.id.mapview)
-        return rootView
-    }
-
-
-    override fun onDrivingRoutes(routes: MutableList<DrivingRoute>) {
-
-    }
-
-    override fun onDrivingRoutesError(error: Error) {
-        val errorMessage = when (error) {
-            is RemoteError -> "Remote error: ${error.isValid}"
-            is NetworkError -> "Network error: Please check your internet connection"
-            else -> "Unknown error"
-        }
-        println("Error getting driving routes: $errorMessage")
-
-    }
-}*/
-
-class ParkGpsFragment : Fragment(), DrivingSession.DrivingRouteListener {
-    private lateinit var mapView: MapView
-    private lateinit var drivingSession: DrivingSession
+    private lateinit var pedestrianRouterSession: Session
     private val start_point = Point(55.752329, 37.609381)
     private val alex_gard=Point(55.752077, 37.613614)
     private val museon=Point(55.733055, 37.604646)
@@ -97,6 +41,7 @@ class ParkGpsFragment : Fragment(), DrivingSession.DrivingRouteListener {
 
         MapKitFactory.initialize(requireContext())
         DirectionsFactory.initialize(requireContext())
+        TransportFactory.initialize(requireContext())
 
 
     }
@@ -127,10 +72,7 @@ class ParkGpsFragment : Fragment(), DrivingSession.DrivingRouteListener {
         mapView.map.mapObjects.addPlacemark(finish_point)
 
         // Запрашиваем маршрут
-        val drivingOptions = DrivingOptions().apply { routesCount = 1 }
-        val vechicleType=VehicleType.TRUCK
-        val vehicleOptions = VehicleOptions()
-        val routerType=DrivingRouterType.ONLINE
+        val timeOptions= TimeOptions()
         val requestPoints = listOf(
             RequestPoint(start_point,RequestPointType.WAYPOINT,null,null),
             RequestPoint(alex_gard,RequestPointType.VIAPOINT,null,null),
@@ -144,10 +86,9 @@ class ParkGpsFragment : Fragment(), DrivingSession.DrivingRouteListener {
 
 
 
-        drivingSession =DirectionsFactory.getInstance().createDrivingRouter(routerType).requestRoutes(
+        pedestrianRouterSession =TransportFactory.getInstance().createPedestrianRouter().requestRoutes(
             requestPoints,
-            drivingOptions,
-            vehicleOptions,
+            timeOptions,
             this
         )
 
@@ -160,26 +101,20 @@ class ParkGpsFragment : Fragment(), DrivingSession.DrivingRouteListener {
         MapKitFactory.getInstance().onStart()
     }
 
-    override fun onDrivingRoutes(routes: MutableList<DrivingRoute>) {
+    override fun onMasstransitRoutes(routes: MutableList<Route>) {
         if (routes.isNotEmpty()) {
-            // Отображаем первый маршрут
             val route = routes[0]
             mapView.map.mapObjects.addPolyline(route.geometry)
         }
     }
 
-    override fun onDrivingRoutesError(error: Error) {
+    override fun onMasstransitRoutesError(error: Error) {
         val errorMessage = when (error) {
             is RemoteError -> "Remote error: ${error.isValid}"
             is NetworkError -> "Network error: Please check your internet connection"
             else -> "Unknown error"
         }
         println("Error getting driving routes: $errorMessage")
-
     }
 
-
-
 }
-
-
